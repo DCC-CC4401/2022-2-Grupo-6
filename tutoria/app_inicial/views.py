c4762ab1b3a726e954d62b7fe88e9f7cc4d0d203
Danymateo777 <danymateo777@gmail.com>
2022-10-04 09:27:25 -0300

    registro-login

--- tutoria/app_inicial/views.py
+++ tutoria/app_inicial/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from app_inicial.models import User
+from django.contrib.auth import authenticate, login,logout
+
+def index(request):
+    return render(request,"registration/index.html")
+
+def register_user(request):
+    if request.method == 'GET': #Si estamos cargando la página
+     return render(request, "registration/register_user.html") #Mostrar el template
+
+    elif request.method == 'POST': #Si estamos recibiendo el form de registro
+        #Tomar los elementos del formulario que vienen en request.POST
+        nombre = request.POST['nombre']
+        contrasena = request.POST['contrasena']
+        mail = request.POST['correo']
+
+        #Crear el nuevo usuario
+        user = User.objects.create_user(username=nombre, password=contrasena, email=mail)
+
+        #Redireccionar la página /tareas
+        return HttpResponseRedirect('/')
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request,"registration/login.html")  
+    if request.method == 'POST':
+        username = request.POST['nombre']
+        contrasena = request.POST['contrasena']
+        usuario = authenticate(username=username,password=contrasena)
+        if usuario is not None:
+            login(request,usuario)
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/accounts/register')
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
--- tutoria/tutoria/urls.py
+++ tutoria/tutoria/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from app_inicial.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include("app_inicial.urls")),
+    path('',index),
 ]
